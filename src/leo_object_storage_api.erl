928e600625d03656bdc6a30ae12885bb462a782a
Yosuke Hara <yousuke.hara@gmail.com>
2014-07-31 15:58:29 +0900

    Fix wrong return type

--- src/leo_object_storage_api.erl
+++ src/leo_object_storage_api.erl
@@ -100,7 +100,7 @@ delete(AddrIdAndKey, Object) ->
 %% @doc Retrieve a metadata from the object-storage
 %%
 -spec(head(tuple()) ->
-             {ok, metadata} | not_found | {error, any()}).
+             {ok, binary()} | not_found | {error, any()}).
 head(AddrIdAndKey) ->
     do_request(head, [AddrIdAndKey]).
 
