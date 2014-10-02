06bc8e374a5c529ad4d694095080159ea30cac33
Yosuke Hara <yousuke.hara@gmail.com>
2014-10-02 11:37:08 +0900

    Modified for making docs

--- make_rst_doc.sh
+++ make_rst_doc.sh
@@ -8,3 +8,18 @@ pandoc --read=html --write=rst doc/leo_object_storage_server.html -o doc/rst/leo
 pandoc --read=html --write=rst doc/leo_object_storage_transformer.html -o doc/rst/leo_object_storage_transformer.rst
 pandoc --read=html --write=rst doc/leo_compact_fsm_controller.html -o doc/rst/leo_compact_fsm_controller.rst
 pandoc --read=html --write=rst doc/leo_compact_fsm_worker.html -o doc/rst/leo_compact_fsm_worker.rst
+
+for filename in doc/rst/leo_object_storage_api.rst \
+                doc/rst/leo_object_storage_haystack.rst \
+                doc/rst/leo_object_storage_server.rst \
+                doc/rst/leo_object_storage_transformer.rst \
+                doc/rst/leo_compact_fsm_controller.rst \
+                doc/rst/leo_compact_fsm_worker.rst
+do
+    sed -ie "1,6d" "$filename"
+    sed -ie "s/\Module //" "$filename"
+    LINE_1=`cat $filename | wc -l`
+    LINE_2=`expr $LINE_1 - 10`
+    sed -ie "$LINE_2,\$d" "$filename"
+done
+rm doc/rst/*.rste
